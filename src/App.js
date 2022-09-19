a908fd679a47ccc2dd75f86452c6d88b3abef84a
Eugenio <eugenio.calabresi@nimisconsulting.com>
2022-09-19 16:49:41 +0200

    add features and style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+
+import './styles.css'
+import React, { useEffect, useState } from "react";
 import Field from "./components/field";
 import Translate from "./components/translate";
 import Languages from "./components/languages";
 
 export default function App() {
+
+  const [language, setLanguage] = useState('es');
+  const [text, setText] = useState('');
+
+  useEffect( () => {
+    console.log('language: '+ language);
+    console.log('text: '+ text);
+  },[language, text])
+
   return (
+
     <div>
-      <h3>Hi there!</h3>
+      <Field onChange={setText} />
+      <Languages language={language} onLanguageChange={setLanguage}/>
+      <hr />
+      <Translate text={text} language={language}/>
     </div>
   );
 }
